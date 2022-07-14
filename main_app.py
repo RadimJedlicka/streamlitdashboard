cef44693fe9ae493fa964a915c74240e19c65f65
RadimJedlicka <95315306+RadimJedlicka@users.noreply.github.com>
2022-07-14 19:18:50 +0200

    Pridani radio buttonu

--- main_app.py
+++ main_app.py
@@ -10,5 +10,5 @@ page = st.sidebar.radio('Select page', ['Test', 'Thomson'])
 if page  == 'Test':
     st.write('Toto je moje prvni aplikace, kterou delam. Dalsi radky budou supr COOL!')
 
-if page == 'Thomson'
+if page == 'Thomson':
     st.write('Thomson sampling')
