e7c074b8b2d5e26a423a49c62d7dce11d316de1b
RadimJedlicka <95315306+RadimJedlicka@users.noreply.github.com>
2022-07-18 11:29:23 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -1,14 +1,33 @@
 import streamlit as st
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+import plotly.graph_objects as go
+import plotly.express as px
+
+# #################
+# Data
+# #################
+
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+
+df_bikes = pd.read_sql(sql='select * from edinburgh_bikes', con=engine)
+
+# #################
+# Vizualizace
+# #################
 
 st.title('Moje prvni appka')
 
-st.write('Toto je moje prvni appka')
-
-page = st.sidebar.radio('Select page', ['Test', 'Thomson'])
+page = st.sidebar.radio('Select page', ['Mapa', 'Thomson'])
 
 
-if page == 'Test':
-    st.write('Toto je moje prvni appka')
+if page == 'Mapa':
+    st.write('Mapa pouzivani sdilenych kol v Edinburghu')
+    fig = px.scatter_mapbox(df_bikes,lat='start_station_latitude', lon='start_station_longitude')
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.show()
+
 
 if page == 'Thomson':
     st.write('thomson sampling')
